c41d2074f0fed4af0be9eb8150556fdb67640f01
Jonathan Clark <jonathan.clark@halter.co.nz>
2025-07-30 13:44:34 +1200

    Add support for 64-bit ticks on ARM CM0 and CM33

--- portable/GCC/ARM_CM33_NTZ/non_secure/portmacrocommon.h
+++ portable/GCC/ARM_CM33_NTZ/non_secure/portmacrocommon.h
@@ -84,6 +84,9 @@ typedef unsigned long    UBaseType_t;
 /* 32-bit tick type on a 32-bit architecture, so reads of the tick count do
  * not need to be guarded with a critical section. */
     #define portTICK_TYPE_IS_ATOMIC    1
+#elif (configTICK_TYPE_WIDTH_IN_BITS == TICK_TYPE_WIDTH_64_BITS)
+    typedef uint64_t        TickType_t;
+    #define portMAX_DELAY  (TickType_t)0xffffffffffffffffULL
 #else
     #error configTICK_TYPE_WIDTH_IN_BITS set to unsupported tick type width.
 #endif
